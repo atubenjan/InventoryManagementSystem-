f2f7aef6bbde8b36cdbc3b945a6b96a50cda2603
Atugonza Benjamin <153981563+atubenjan@users.noreply.github.com>
2024-08-23 01:36:13 +0300

    edits
    edits

---
+++ footer.php
@@ -0,0 +1,26 @@
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2024 <a href="#">Paint & Sip</a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 1
+    </div>
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+
+<script src="plugins/jquery/jquery.min.js"></script>
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="dist/js/adminlte.js"></script>
+<script src="plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="plugins/raphael/raphael.min.js"></script>
+<script src="plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="plugins/jquery-mapael/maps/usa_states.min.js"></script>
+<script src="plugins/chart.js/Chart.min.js"></script>
+
+<script src="dist/js/pages/dashboard2.js"></script>
+</body>
+</html>
